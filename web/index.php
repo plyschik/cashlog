16e058cf84fe70aaefdaa11df7c8107906bb6bd0
plyschik <palyschik@gmail.com>
2017-07-23 18:11:18 +0200

    Doctrine DBAL support added.

--- web/index.php
+++ web/index.php
@@ -20,6 +20,16 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), [
         'cache' => __DIR__ . '/../var/cache'
     ]
 ]);
+$app->register(new \Silex\Provider\DoctrineServiceProvider(), [
+   'db.options' => [
+       'driver'     => getenv('DATABASE_DRIVER'),
+       'host'       => getenv('DATABASE_HOST'),
+       'dbname'     => getenv('DATABASE_NAME'),
+       'user'       => getenv('DATABASE_USER'),
+       'password'   => getenv('DATABASE_PASSWORD'),
+       'charset'    => getenv('DATABASE_CHARSET')
+   ] 
+]);
 
 $app['SecurityController'] = function () use ($app) {
     return new SecurityController($app);
