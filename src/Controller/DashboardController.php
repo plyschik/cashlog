683233e278eefd850ad866124bed7b3edbeb3d86
plyschik <palyschik@gmail.com>
2017-07-25 16:36:40 +0200

    Dashboard form constraints.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -15,30 +15,51 @@ class DashboardController extends BaseController
         $form = $this->app['form.factory']->createBuilder()
             ->add('type', ChoiceType::class, [
                 'label' => false,
+                'invalid_message' => 'Wybrany typ operacji jest niepoprawny.',
                 'choices' => [
                     'WPŁATA' => 0,
                     'WYPŁATA' => 1
                 ],
-                'constraints' => new Assert\Choice([
-                    'choices' => [0, 1]
-                ])
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Wybrany typ operacji nie istnieje.'
+                    ]),
+                    new Assert\Choice([
+                        'choices' => [0, 1],
+                        'message' => 'Wybierz poprawny typ operacji.'
+                    ])
+                ]
             ])
             ->add('description', TextType::class, [
                 'label' => false,
                 'constraints' => [
                     new Assert\NotBlank([
                         'message' => 'Opis jest polem wymaganym.'
+                    ]),
+                    new Assert\Length([
+                        'min' => 8,
+                        'max' => 64,
+                        'minMessage' => 'Opis musi zawierać conajmniej 8 znaków.',
+                        'maxMessage' => 'Opis może mieć maksymalnie 64 znaki.'
                     ])
                 ]
             ])
-            ->add('cash', MoneyType::class, [
+            ->add('cash', TextType::class, [
                 'label' => false,
+                'invalid_message' => 'Wpisana kwota jest niepoprawna.',
                 'constraints' => [
                     new Assert\NotBlank([
                         'message' => 'Kwota jest polem wymaganym.'
+                    ]),
+                    new Assert\GreaterThan([
+                        'value' => 0,
+                        'message' => 'Kwota powinna być liczbą większą od zera.'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]{1,6}(?:\.[0-9]{0,2})?$/',
+                        'message' => 'Wpisana kwota nie pasuje do wzoru. Kropka zamiast przecinka.'
                     ])
-                ],
-                'currency' => ''
+                ]
             ])
             ->getForm()
         ;
